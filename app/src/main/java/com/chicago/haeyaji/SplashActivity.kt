4ba2251bcc7b5feacf79c479732a0901d4a21bb1
visionCreateNewSun <rufari29@gmail.com>
2024-11-28 23:02:19 +0900

    [feature/splash] 스플래시 스크린 기본 구현
    * 상세 로직은 추후 구현

---
+++ app/src/main/java/com/chicago/haeyaji/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.chicago.haeyaji
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+
+    val activityScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        moveToMain()
+    }
+
+    private fun moveToMain(){
+        activityScope.launch {
+            delay(2000)
+
+            var intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
